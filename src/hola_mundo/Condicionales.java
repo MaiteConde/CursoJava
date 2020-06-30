32d613934d3235a6fb5e65b46d8992a81196cc2f
Maite <maitecondepalacio@gmail.com>
2020-06-30 13:36:29 +0200

    primeros condicionales

---
+++ src/hola_mundo/Condicionales.java
@@ -0,0 +1,17 @@
+package hola_mundo;
+
+import  java.util.*;
+public class Condicionales {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce tu edad");
+        int edad = entrada.nextInt();
+        if (edad >= 18){
+            System.out.println("Eres mayor de edad");
+        }
+
+            System.out.println("No eres mayor de edad");
+
+
+    }
+}
