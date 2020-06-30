087810b28eaa1aa87099be291e84bf835e07157e
Maite <maitecondepalacio@gmail.com>
2020-06-30 12:31:25 +0200

    declaraciones y operadores

---
+++ src/hola_mundo/Declaraciones.java
@@ -0,0 +1,17 @@
+package hola_mundo;
+
+public class Declaraciones {
+    public static void main(String[] args) {
+
+        final double apulgadas = 2.54;
+        double cm = 6;
+        double resultado = cm/apulgadas;
+        System.out.print("En " + cm + " cm hay "+ resultado + " pulgadas");
+
+        int a,b,c;
+        a=2;
+        b=3;
+        c=a+b;
+        System.out.print(c);
+    }
+}
--- src/hola_mundo/Variables.java
+++ src/hola_mundo/Variables.java
@@ -3,8 +3,7 @@ package hola_mundo;
 public class Variables {
     public static void main(String[] args) {
         byte edad = 27;
-        edad = 30;
-        System.out.print(edad);
 
+        System.out.print(edad);
     }
 }
