eecaf52918d7bca6b06d4a670ab51c472d09fb67
Maite <maitecondepalacio@gmail.com>
2020-06-30 12:08:03 +0200

    primera variable

---
+++ src/hola_mundo/Variables.java
@@ -0,0 +1,10 @@
+package hola_mundo;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte edad = 27;
+        edad = 30;
+        System.out.print(edad);
+
+    }
+}
